15516d5c60261bee7c76f9cf67b7dee05b9b4658
Ganesh <GRajamahant@anglianwater.co.uk>
2024-02-12 16:40:38 +0000

    Updated Program class

--- src/ncea-harvester/Program.cs
+++ src/ncea-harvester/Program.cs
@@ -37,80 +37,79 @@ ConfigureProcessor(builder, configuration);
 var host = builder.Build();
 host.Run();
 
-static void ConfigureProcessor(HostApplicationBuilder builder, IConfiguration configuration)
-{
-    var processorTypeName = configuration.GetValue<string>("HarvesterConfigurations:Processor:Type");
-    var assembly = typeof(Program).Assembly;
-    var type = assembly.GetType(processorTypeName!);
-
-    if (type != null)
-    {
-        builder.Services.AddSingleton(typeof(IProcessor), type);
-    }
-}
-
-static async Task ConfigureServiceBusQueue(IConfigurationRoot configuration, HostApplicationBuilder builder, string dataSourceName)
-{
-    var servicebusHostName = configuration.GetValue<string>("ServiceBusHostName");
-    builder.Services.AddSingleton(x => new ServiceBusClient(servicebusHostName, new DefaultAzureCredential()));    
-    
-    var queueName = $"{dataSourceName}-harvester-queue";
-
-    var servicebusAdminClient = new ServiceBusAdministrationClient(servicebusHostName, new DefaultAzureCredential());
-    bool queueExists = await servicebusAdminClient.QueueExistsAsync(queueName);
-    if (!queueExists)
-    {
-        await servicebusAdminClient.CreateQueueAsync(queueName);
-    }
-}
-
-static void ConfigureKeyVault(IConfigurationRoot configuration, HostApplicationBuilder builder)
-{
-    var keyVaultEndpoint = new Uri(configuration.GetValue<string>("KeyVaultUri")!);
-    builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-    builder.Services.AddSingleton(x => new SecretClient(keyVaultEndpoint, new DefaultAzureCredential()));
-}
-
-static void ConfigureLogging(HostApplicationBuilder builder)
-{
-    builder.Services.AddLogging(loggingBuilder =>
-    {
-        loggingBuilder.ClearProviders();
-        loggingBuilder.AddApplicationInsights(
-            configureTelemetryConfiguration: (config) =>
-                config.ConnectionString = builder.Configuration.GetValue<string>("ApplicationInsights:ConnectionString"),
-                configureApplicationInsightsLoggerOptions: (options) => { }
-            );
-        loggingBuilder.AddFilter<ApplicationInsightsLoggerProvider>(null, LogLevel.Information);
-
-    });
-    builder.Services.AddApplicationInsightsTelemetryWorkerService();
-    builder.Services.ConfigureTelemetryModule<DependencyTrackingTelemetryModule>((module, o) =>
-    {
-        module.EnableSqlCommandTextInstrumentation = true;
-        o.ConnectionString = builder.Configuration.GetValue<string>("ApplicationInsights:ConnectionString");
-    });
-}
-
-static async Task ConfigureBlobStorage(IConfigurationRoot configuration, HostApplicationBuilder builder, string dataSourceName)
-{
-    var blobStorageEndpoint = new Uri(configuration.GetValue<string>("BlobStorageUri")!);
-    var blobServiceClient = new BlobServiceClient(blobStorageEndpoint, new DefaultAzureCredential());
-
-    builder.Services.AddSingleton(x => blobServiceClient);
-    BlobContainerClient container = blobServiceClient.GetBlobContainerClient(dataSourceName);
-    await container.CreateIfNotExistsAsync();
-}
-
-static void ConfigureServices(HostApplicationBuilder builder)
-{
-    builder.Services.AddSingleton<IApiClient, ApiClient>();
-    builder.Services.AddSingleton<IServiceBusService, ServiceBusService>();
-    builder.Services.AddSingleton<IKeyVaultService, KeyVaultService>();
-    builder.Services.AddSingleton<IBlobService, BlobService>();
-}
-
-
 
 [ExcludeFromCodeCoverage]
-public static partial class Program { }
+public static partial class Program {
+    static void ConfigureProcessor(HostApplicationBuilder builder, IConfiguration configuration)
+    {
+        var processorTypeName = configuration.GetValue<string>("HarvesterConfigurations:Processor:Type");
+        var assembly = typeof(Program).Assembly;
+        var type = assembly.GetType(processorTypeName!);
+
+        if (type != null)
+        {
+            builder.Services.AddSingleton(typeof(IProcessor), type);
+        }
+    }
+
+    static async Task ConfigureServiceBusQueue(IConfigurationRoot configuration, HostApplicationBuilder builder, string dataSourceName)
+    {
+        var servicebusHostName = configuration.GetValue<string>("ServiceBusHostName");
+        builder.Services.AddSingleton(x => new ServiceBusClient(servicebusHostName, new DefaultAzureCredential()));
+
+        var queueName = $"{dataSourceName}-harvester-queue";
+
+        var servicebusAdminClient = new ServiceBusAdministrationClient(servicebusHostName, new DefaultAzureCredential());
+        bool queueExists = await servicebusAdminClient.QueueExistsAsync(queueName);
+        if (!queueExists)
+        {
+            await servicebusAdminClient.CreateQueueAsync(queueName);
+        }
+    }
+
+    static void ConfigureKeyVault(IConfigurationRoot configuration, HostApplicationBuilder builder)
+    {
+        var keyVaultEndpoint = new Uri(configuration.GetValue<string>("KeyVaultUri")!);
+        builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+        builder.Services.AddSingleton(x => new SecretClient(keyVaultEndpoint, new DefaultAzureCredential()));
+    }
+
+    static void ConfigureLogging(HostApplicationBuilder builder)
+    {
+        builder.Services.AddLogging(loggingBuilder =>
+        {
+            loggingBuilder.ClearProviders();
+            loggingBuilder.AddApplicationInsights(
+                configureTelemetryConfiguration: (config) =>
+                    config.ConnectionString = builder.Configuration.GetValue<string>("ApplicationInsights:ConnectionString"),
+                    configureApplicationInsightsLoggerOptions: (options) => { }
+                );
+            loggingBuilder.AddFilter<ApplicationInsightsLoggerProvider>(null, LogLevel.Information);
+
+        });
+        builder.Services.AddApplicationInsightsTelemetryWorkerService();
+        builder.Services.ConfigureTelemetryModule<DependencyTrackingTelemetryModule>((module, o) =>
+        {
+            module.EnableSqlCommandTextInstrumentation = true;
+            o.ConnectionString = builder.Configuration.GetValue<string>("ApplicationInsights:ConnectionString");
+        });
+    }
+
+    static async Task ConfigureBlobStorage(IConfigurationRoot configuration, HostApplicationBuilder builder, string dataSourceName)
+    {
+        var blobStorageEndpoint = new Uri(configuration.GetValue<string>("BlobStorageUri")!);
+        var blobServiceClient = new BlobServiceClient(blobStorageEndpoint, new DefaultAzureCredential());
+
+        builder.Services.AddSingleton(x => blobServiceClient);
+        BlobContainerClient container = blobServiceClient.GetBlobContainerClient(dataSourceName);
+        await container.CreateIfNotExistsAsync();
+    }
+
+    static void ConfigureServices(HostApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<IApiClient, ApiClient>();
+        builder.Services.AddSingleton<IServiceBusService, ServiceBusService>();
+        builder.Services.AddSingleton<IKeyVaultService, KeyVaultService>();
+        builder.Services.AddSingleton<IBlobService, BlobService>();
+    }
+}
