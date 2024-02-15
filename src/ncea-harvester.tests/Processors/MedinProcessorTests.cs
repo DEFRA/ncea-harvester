5887b0c5eb7b9520b56c88969cce0d66b6285530
Ganesh <GRajamahant@anglianwater.co.uk>
2024-02-15 19:35:07 +0000

    Code changes 1. Medin processor code changes 2. Unit Tests

--- src/ncea-harvester.tests/Processors/MedinProcessorTests.cs
+++ src/ncea-harvester.tests/Processors/MedinProcessorTests.cs
@@ -14,6 +14,83 @@ namespace Ncea.Harvester.Tests.Processors;
 public class MedinProcessorTests
 {
     [Fact]
+    public async Task Process_ShouldSendMessagesToServiceBus()
+    {
+        //Arrange
+        var serviceBusService = ServiceBusServiceForTests.Get(out Mock<ServiceBusSender> mockServiceBusSender);
+        string expectedData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                            "<csw:GetRecordsResponse xmlns:csw=\"http://www.opengis.net/cat/csw/2.0.2\">" +
+                            "  <csw:SearchStatus timestamp=\"2024-02-15T17:54:36.664Z\" />" +
+                            "  <csw:SearchResults numberOfRecordsMatched=\"2\" numberOfRecordsReturned=\"2\" elementSet=\"full\" nextRecord=\"3\">" +
+                            "    <gmd:MD_Metadata xmlns:gmd=\"http://www.isotc211.org/2005/gmd\" xmlns:gco=\"http://www.isotc211.org/2005/gco\">" +
+                            "      <gmd:fileIdentifier>" +
+                            "        <gco:CharacterString>abce1a60-c7f2-42fd-81e9-03d54ab01f0f</gco:CharacterString>" +
+                            "      </gmd:fileIdentifier>" +
+                            "    </gmd:MD_Metadata>" +
+                            "    <gmd:MD_Metadata xmlns:gmd=\"http://www.isotc211.org/2005/gmd\" xmlns:gco=\"http://www.isotc211.org/2005/gco\">" +
+                            "      <gmd:fileIdentifier>" +
+                            "        <gco:CharacterString>defe1a60-c7f2-42fd-81e9-03d54ab01f0f</gco:CharacterString>" +
+                            "      </gmd:fileIdentifier>" +
+                            "    </gmd:MD_Metadata>" +
+                            "  </csw:SearchResults>" +
+                            "</csw:GetRecordsResponse>";
+        var httpResponse = new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent(expectedData),
+        };
+        var apiClient = ApiClientForTests.Get(httpResponse);
+        var appSettings = Options.Create(new HarvesterConfigurations() { Processor = new Processor() { DataSourceApiBase = "https://base-uri", DataSourceApiUrl = "/test-url", ProcessorType = ProcessorType.Jncc, Type = "" } });
+        var blobService = BlobServiceForTests.Get(out Mock<BlobServiceClient> mockBlobServiceClient,
+                                              out Mock<BlobContainerClient> mockBlobContainerClient,
+                                              out Mock<BlobClient> mockBlobClient);
+        var logger = new Logger<MedinProcessor>(new LoggerFactory());
+        // Act
+        var medinService = new MedinProcessor(apiClient, serviceBusService, blobService, logger, appSettings);
+        await medinService.Process();
+
+        // Assert
+        mockServiceBusSender.Verify(x => x.SendMessageAsync(It.IsAny<ServiceBusMessage>(), default), Times.Exactly(2));
+        mockBlobServiceClient.Verify(x => x.GetBlobContainerClient(It.IsAny<string>()), Times.Exactly(2));
+        mockBlobContainerClient.Verify(x => x.GetBlobClient(It.IsAny<string>()), Times.Exactly(2));
+        mockBlobClient.Verify(x => x.UploadAsync(It.IsAny<Stream>(), It.IsAny<bool>(), It.IsAny<CancellationToken>()), Times.Exactly(2));
+    }
+
+    [Fact]
+    public async Task Process_ShouldNotSendMessagesToServiceBus()
+    {
+        //Arrange
+        var serviceBusService = ServiceBusServiceForTests.Get(out Mock<ServiceBusSender> mockServiceBusSender);
+        var expectedData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                           "<ows:ExceptionReport xmlns:ows=\"http://www.opengis.net/ows\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"1.2.0\" xsi:schemaLocation=\"http://www.opengis.net/ows http://schemas.opengis.net/ows/1.0.0/owsExceptionReport.xsd\">" +
+                           "  <ows:Exception exceptionCode=\"NoApplicableCode\">" +
+                           "    <ows:ExceptionText>java.lang.RuntimeException: org.fao.geonet.csw.common.exceptions.InvalidParameterValueEx: code=InvalidParameterValue, locator=startPosition, message=Start position (17804) can't be greater than number of matching records (17803 for current search).</ows:ExceptionText>" +
+                           "  </ows:Exception>" +
+                           "</ows:ExceptionReport>";
+        var httpResponse = new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent(expectedData),
+        };
+        var apiClient = ApiClientForTests.Get(httpResponse);
+        var appSettings = Options.Create(new HarvesterConfigurations() { Processor = new Processor() { DataSourceApiBase = "https://base-uri", DataSourceApiUrl = "/test-url", ProcessorType = ProcessorType.Jncc, Type = "" } });
+        var blobServiceMock = BlobServiceForTests.Get(out Mock<BlobServiceClient> mockBlobServiceClient,
+                                                      out Mock<BlobContainerClient> mockBlobContainerClient,
+                                                      out Mock<BlobClient> mockBlobClient);
+        var loggerMock = new Mock<ILogger<MedinProcessor>>();
+
+        // Act
+        var medinService = new MedinProcessor(apiClient, serviceBusService, blobServiceMock, loggerMock.Object, appSettings);
+        await medinService.Process();
+
+        // Assert
+        mockServiceBusSender.Verify(x => x.SendMessageAsync(It.IsAny<ServiceBusMessage>(), default), Times.Never);
+        mockBlobServiceClient.Verify(x => x.GetBlobContainerClient(It.IsAny<string>()), Times.Never);
+        mockBlobContainerClient.Verify(x => x.GetBlobClient(It.IsAny<string>()), Times.Never);
+        mockBlobClient.Verify(x => x.UploadAsync(It.IsAny<Stream>(), It.IsAny<bool>(), It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
     public async Task Process_ShouldThrowError()
     {
         //Arrange
@@ -28,9 +105,10 @@ public class MedinProcessorTests
                                               out Mock<BlobContainerClient> mockBlobContainerClient,
                                               out Mock<BlobClient> mockBlobClient);
         var logger = new Logger<MedinProcessor>(new LoggerFactory());
+
+
         // Act & Assert
         var medinService = new MedinProcessor(apiClient, serviceBusService, blobService,logger, appSettings);
-        await Assert.ThrowsAsync<NotImplementedException>(() => medinService.Process());
-        //await Assert.ThrowsAsync<HttpRequestException>(() => medinService.Process());        
+        await Assert.ThrowsAsync<HttpRequestException>(() => medinService.Process());        
     }
 }
--- src/ncea-harvester/Processor/MedinProcessor.cs
+++ src/ncea-harvester/Processor/MedinProcessor.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Extensions.Options;
 using Ncea.Harvester.Infrastructure.Contracts;
+using Ncea.Harvester.Infrastructure.Models.Requests;
 using Ncea.Harvester.Models;
 using Ncea.Harvester.Processors.Contracts;
+using System.Text;
+using System.Xml.Linq;
 
 namespace Ncea.Harvester.Processors;
 
@@ -26,14 +29,94 @@ public class MedinProcessor : IProcessor
         _logger = logger;
         _blobService = blobService;
     }
-    public Task Process()
+    public async Task Process()
     {
-        Console.Write(_harvesterConfigurations);
-        Console.Write(_apiClient);
-        Console.Write(_serviceBusService);
-        Console.Write(_blobService);
-        Console.Write(_logger);
-        Console.Write(_apiClient);
-        throw new NotImplementedException();
+        var startPosition = 1;
+        var maxRecords = 100;
+        var totalRecords = 0;
+        var hasNextRecords = true;
+        var hasTotalRecords = true;
+
+        while (hasNextRecords)
+        {
+            var responseXml = await GetMedinData(startPosition, maxRecords);
+            if (responseXml == null) continue;
+
+            startPosition = GetNextStartPostionInMedinData(out hasNextRecords, out hasTotalRecords, out totalRecords, responseXml);
+            if (!hasNextRecords) continue;
+
+            var metaDataXmlNodes = GetMetadataList(responseXml);
+            if (metaDataXmlNodes == null || !metaDataXmlNodes.Any()) continue;
+
+            await SendMetaDataToServiceBus(metaDataXmlNodes);
+            hasNextRecords = (startPosition <= totalRecords);
+        }
+    }
+
+    private async Task SendMetaDataToServiceBus(IEnumerable<XElement> metaDataXmlNodes)
+    {
+        foreach (var metaDataXmlNode in metaDataXmlNodes)
+            {
+                try
+                {
+                    var metaDataXmlString = Convert.ToString(metaDataXmlNode);
+                    if (string.IsNullOrWhiteSpace(metaDataXmlString)) continue;
+
+                    await _serviceBusService.SendMessageAsync(metaDataXmlString);
+                    var xmlStream = new MemoryStream(Encoding.ASCII.GetBytes(metaDataXmlString));
+                    var dataSourceName = _harvesterConfigurations.Processor.ProcessorType.ToString().ToLowerInvariant();
+
+                    var documentFileIdentifier = GetFileIdentifier(metaDataXmlNode);
+                    var documentFileName = string.Concat(documentFileIdentifier, ".xml");
+                    await _blobService.SaveAsync(new SaveBlobRequest(xmlStream, Path.GetFileName(documentFileName), dataSourceName), CancellationToken.None);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Error occured while sending message to harvester queue");
+                }
+            }
+    }
+
+    private static IEnumerable<XElement>? GetMetadataList(XDocument? responseXml)
+    {
+        string gmdNameSpaceString = "http://www.isotc211.org/2005/gmd";
+        var metaDataXmlElements = responseXml?.Descendants()
+                               .Where(n => n.Name.Namespace.NamespaceName == gmdNameSpaceString
+                                           && n.Name.LocalName == "MD_Metadata");
+        return metaDataXmlElements;
+    }
+
+    private static string? GetFileIdentifier(XElement? xmlElement)
+    {
+        string gmdNameSpaceString = "http://www.isotc211.org/2005/gmd";
+        var fileIdentifierXmlElement = xmlElement?.Descendants()
+                               .Where(n => n.Name.Namespace.NamespaceName == gmdNameSpaceString
+                                           && n.Name.LocalName == "fileIdentifier").FirstOrDefault();
+        var fileIdentifier = fileIdentifierXmlElement?.Descendants()?.FirstOrDefault()?.Value;
+        return fileIdentifier;
+    }
+
+    private static int GetNextStartPostionInMedinData(out bool hasNextRecords, out bool hasTotalRecords, out int totalRecords, XDocument? responseXml)
+    {
+        var cswNameSpace = "http://www.opengis.net/cat/csw/2.0.2";
+        var searchResultsElement = responseXml?.Descendants()
+                                        .Where(n => n.Name.Namespace.NamespaceName == cswNameSpace
+                                                    && n.Name.LocalName == "SearchResults")
+                                        .FirstOrDefault();
+        var nextRecordAttribute = searchResultsElement?.Attribute("nextRecord")?.Value;
+        var totalRecordAttribute = searchResultsElement?.Attribute("numberOfRecordsMatched")?.Value;
+        hasNextRecords = Int32.TryParse(nextRecordAttribute, out int nextRecord);
+        hasTotalRecords = Int32.TryParse(totalRecordAttribute, out totalRecords);
+        hasNextRecords = (hasTotalRecords && hasNextRecords && nextRecord > 0);
+        return nextRecord;
+    }
+
+    private async Task<XDocument?> GetMedinData(int startPosition, int maxRecords)
+    {
+        var apiUrl = _harvesterConfigurations.Processor.DataSourceApiUrl;
+        apiUrl = apiUrl.Replace("{{maxRecords}}", Convert.ToString(maxRecords)).Replace("{{startPosition}}", Convert.ToString(startPosition));
+        var responseXmlString = await _apiClient.GetAsync(apiUrl);
+        var responseXml = XDocument.Parse(responseXmlString);
+        return responseXml;
     }
 }
