65bef9a0aa07332d256754e3f41012cc60f67684
Ganesh Rajamahanthy <149069705+ganesh-rajamahanthy@users.noreply.github.com>
2024-02-19 10:38:06 +0000

    Feature/293064 medin harvester service (#2)
    * Code changes
    1. Medin processor code changes
    2. Unit Tests
    
    ---------
    
    Co-authored-by: Ganesh <GRajamahant@anglianwater.co.uk>

--- src/ncea-harvester/Processor/MedinProcessor.cs
+++ src/ncea-harvester/Processor/MedinProcessor.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Extensions.Options;
 using Ncea.Harvester.Infrastructure.Contracts;
+using Ncea.Harvester.Infrastructure.Models.Requests;
 using Ncea.Harvester.Models;
 using Ncea.Harvester.Processors.Contracts;
+using System.Text;
+using System.Xml.Linq;
 
 namespace Ncea.Harvester.Processors;
 
@@ -26,14 +29,93 @@ public class MedinProcessor : IProcessor
         _logger = logger;
         _blobService = blobService;
     }
-    public Task Process()
+    public async Task Process()
     {
-        Console.Write(_harvesterConfigurations);
-        Console.Write(_apiClient);
-        Console.Write(_serviceBusService);
-        Console.Write(_blobService);
-        Console.Write(_logger);
-        Console.Write(_apiClient);
-        throw new NotImplementedException();
+        var startPosition = 1;
+        var maxRecords = 100;
+        var totalRecords = 0;
+        var hasNextRecords = true;
+
+        while (hasNextRecords)
+        {
+            var responseXml = await GetMedinData(startPosition, maxRecords);
+            startPosition = GetNextStartPostionInMedinData(out hasNextRecords, out totalRecords, responseXml);
+            var metaDataXmlNodes = GetMetadataList(responseXml, hasNextRecords);
+            await SendMetaDataToServiceBus(metaDataXmlNodes);
+
+            if(startPosition != 0) hasNextRecords = (startPosition <= totalRecords);
+        }
+    }
+
+    private async Task SendMetaDataToServiceBus(IEnumerable<XElement>? metaDataXmlNodes)
+    {
+        if (metaDataXmlNodes == null || !metaDataXmlNodes.Any()) 
+          return; 
+        
+        foreach (var metaDataXmlNode in metaDataXmlNodes)
+        {
+            try
+            {
+                string? metaDataXmlString = metaDataXmlNode.ToString();
+                await _serviceBusService.SendMessageAsync(metaDataXmlString);
+                var xmlStream = new MemoryStream(Encoding.ASCII.GetBytes(metaDataXmlString));
+                var dataSourceName = _harvesterConfigurations.Processor.ProcessorType.ToString().ToLowerInvariant();
+
+                var documentFileIdentifier = GetFileIdentifier(metaDataXmlNode);
+                var documentFileName = string.Concat(documentFileIdentifier, ".xml");
+                await _blobService.SaveAsync(new SaveBlobRequest(xmlStream, documentFileName, dataSourceName), CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error occured while sending message to harvester queue");
+            }
+        }
+    }
+
+    private static List<XElement>? GetMetadataList(XDocument? responseXml, bool hasNextRecords)
+    {
+        List<XElement> metadataList = new List<XElement>();
+        if ((responseXml == null) || !hasNextRecords) 
+            return metadataList;
+
+        
+        string gmdNameSpaceString = "http://www.isotc211.org/2005/gmd";
+        metadataList = responseXml.Descendants()
+                               .Where(n => n.Name.Namespace.NamespaceName == gmdNameSpaceString
+                                           && n.Name.LocalName == "MD_Metadata").ToList();
+        return (metadataList.Count > 0 ? metadataList : new List<XElement>());
+    }
+
+    private static string? GetFileIdentifier(XElement xmlElement)
+    {
+        string gmdNameSpaceString = "http://www.isotc211.org/2005/gmd";
+        var fileIdentifierXmlElement = xmlElement.Descendants()
+                               .FirstOrDefault(n => n.Name.Namespace.NamespaceName == gmdNameSpaceString
+                                           && n.Name.LocalName == "fileIdentifier");
+        var fileIdentifier = fileIdentifierXmlElement?.Descendants()?.FirstOrDefault()?.Value;
+        return fileIdentifier;
+    }
+
+    private static int GetNextStartPostionInMedinData(out bool hasNextRecords, out int totalRecords, XDocument responseXml)
+    {
+        var cswNameSpace = "http://www.opengis.net/cat/csw/2.0.2";
+        var searchResultsElement = responseXml.Descendants()
+                                        .FirstOrDefault(n => n.Name.Namespace.NamespaceName == cswNameSpace
+                                                    && n.Name.LocalName == "SearchResults");
+        var nextRecordAttribute = searchResultsElement?.Attribute("nextRecord")?.Value;
+        var totalRecordAttribute = searchResultsElement?.Attribute("numberOfRecordsMatched")?.Value;
+        hasNextRecords = Int32.TryParse(nextRecordAttribute, out int nextRecord);
+        bool hasTotalRecords = Int32.TryParse(totalRecordAttribute, out totalRecords);
+        hasNextRecords = (hasTotalRecords && hasNextRecords && nextRecord > 0);
+        return nextRecord;
+    }
+
+    private async Task<XDocument> GetMedinData(int startPosition, int maxRecords)
+    {
+        var apiUrl = _harvesterConfigurations.Processor.DataSourceApiUrl;
+        apiUrl = apiUrl.Replace("{{maxRecords}}", Convert.ToString(maxRecords)).Replace("{{startPosition}}", Convert.ToString(startPosition));
+        var responseXmlString = await _apiClient.GetAsync(apiUrl);
+        XDocument responseXml = XDocument.Parse(responseXmlString);
+        return responseXml;
     }
 }
