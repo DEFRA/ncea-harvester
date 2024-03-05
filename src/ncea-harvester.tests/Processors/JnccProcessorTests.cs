4a00f04df5b56fb44ae2027469d62df46651074b
CTS\189285 <Janardhanan.SusaiManiPeter@cognizant.com>
2024-03-05 18:51:18 +0100

    utc fixes

--- src/ncea-harvester.tests/Processors/JnccProcessorTests.cs
+++ src/ncea-harvester.tests/Processors/JnccProcessorTests.cs
@@ -87,4 +87,63 @@ public class JnccProcessorTests
         var jnccService = new JnccProcessor(apiClient, serviceBusService, blobService,logger, harvesterConfiguration);
         await Assert.ThrowsAsync<HttpRequestException>(() => jnccService.Process());
     }
+
+    [Fact]
+    public async Task Process_ShouldThrowException()
+    {
+        //Arrange
+        var serviceBusService = ServiceBusServiceForTests.GetServiceBusWithError(out Mock<ServiceBusSender> mockServiceBusSender);
+        string expectedData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                            "<csw:GetRecordsResponse xmlns:csw=\"http://www.opengis.net/cat/csw/2.0.2\">" +
+                            "  <csw:SearchStatus timestamp=\"2024-02-15T17:54:36.664Z\" />" +
+                            "  <csw:SearchResults numberOfRecordsMatched=\"2\" numberOfRecordsReturned=\"2\" elementSet=\"full\" nextRecord=\"3\">" +
+                            "    <gmd:MD_Metadata xmlns:gmd=\"http://www.isotc211.org/2005/gmd\" xmlns:gco=\"http://www.isotc211.org/2005/gco\">" +
+                            "      <gmd:fileIdentifier>" +
+                            "        <gco:CharacterString>abce1a60-c7f2-42fd-81e9-03d54ab01f0f</gco:CharacterString>" +
+                            "      </gmd:fileIdentifier>" +
+                            "    </gmd:MD_Metadata>" +
+                            "    <gmd:MD_Metadata xmlns:gmd=\"http://www.isotc211.org/2005/gmd\" xmlns:gco=\"http://www.isotc211.org/2005/gco\">" +
+                            "      <gmd:fileIdentifier>" +
+                            "        <gco:CharacterString>defe1a60-c7f2-42fd-81e9-03d54ab01f0f</gco:CharacterString>" +
+                            "      </gmd:fileIdentifier>" +
+                            "    </gmd:MD_Metadata>" +
+                            "  </csw:SearchResults>" +
+                            "</csw:GetRecordsResponse>";
+        var httpResponse = new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = new StringContent(expectedData),
+        };
+        var apiClient = ApiClientForTests.Get(httpResponse);
+        var harvesterConfiguration = new HarvesterConfiguration() { DataSourceApiBase = "https://base-uri", DataSourceApiUrl = "/test-url", ProcessorType = ProcessorType.Jncc, Type = "" };
+
+        var blobService = BlobServiceForTests.Get(out Mock<BlobServiceClient> mockBlobServiceClient,
+                                              out Mock<BlobContainerClient> mockBlobContainerClient,
+                                              out Mock<BlobClient> mockBlobClient);
+        var mockLogger = new Mock<ILogger<MedinProcessor>>(MockBehavior.Strict);
+        mockLogger.Setup(x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()
+            )
+        );
+
+        // Act
+        var jnccService = new MedinProcessor(apiClient, serviceBusService, blobService, mockLogger.Object, harvesterConfiguration);
+        await jnccService.Process();
+
+        // Assert
+        mockLogger.Verify(
+            m => m.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Exactly(2),
+            It.IsAny<string>()
+        );
+    }
 }
