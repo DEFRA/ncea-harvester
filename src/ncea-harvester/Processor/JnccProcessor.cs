54d37ead2425da145f1018d639d01d2073c8cf00
CTS\189285 <Janardhanan.SusaiManiPeter@cognizant.com>
2024-03-05 18:25:10 +0100

    fixes

--- src/ncea-harvester/Processor/JnccProcessor.cs
+++ src/ncea-harvester/Processor/JnccProcessor.cs
@@ -4,19 +4,22 @@ using Ncea.Harvester.Infrastructure.Models.Requests;
 using Ncea.Harvester.Models;
 using Ncea.Harvester.Processors.Contracts;
 using System.Text;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Ncea.Harvester.Processors;
 
 public class JnccProcessor : IProcessor
 {
+    private readonly string _dataSourceName;
     private readonly IApiClient _apiClient;
     private readonly IServiceBusService _serviceBusService;
     private readonly IBlobService _blobService;
     private readonly ILogger _logger;
     private readonly HarvesterConfiguration _harvesterConfiguration;
 
-    public JnccProcessor(IApiClient apiClient, 
-        IServiceBusService serviceBusService, 
+    public JnccProcessor(IApiClient apiClient,
+        IServiceBusService serviceBusService,
         IBlobService blobService,
         ILogger<JnccProcessor> logger,
         HarvesterConfiguration harvesterConfiguration)
@@ -27,6 +30,8 @@ public class JnccProcessor : IProcessor
         _serviceBusService = serviceBusService;
         _logger = logger;
         _blobService = blobService;
+
+        _dataSourceName = _harvesterConfiguration.ProcessorType.ToString().ToLowerInvariant();
     }
     public async Task Process()
     {
@@ -39,17 +44,22 @@ public class JnccProcessor : IProcessor
     {
         foreach (var documentLink in documentLinks)
         {
+            var documentFileIdentifier = string.Empty;
             try
             {
                 var apiUrl = "/waf/" + documentLink;
                 var metaDataXmlString = await _apiClient.GetAsync(apiUrl);
                 await _serviceBusService.SendMessageAsync(metaDataXmlString);
+
                 var xmlStream = new MemoryStream(Encoding.ASCII.GetBytes(metaDataXmlString));
-                var dataSourceName = _harvesterConfiguration.ProcessorType.ToString().ToLowerInvariant();
-                await _blobService.SaveAsync(new SaveBlobRequest(xmlStream, Path.GetFileName(documentLink), dataSourceName), CancellationToken.None);                
-            } catch (Exception ex)
+                documentFileIdentifier = GetFileIdentifier(metaDataXmlString);
+                var documentFileName = string.Concat(documentFileIdentifier, ".xml");
+
+                await _blobService.SaveAsync(new SaveBlobRequest(xmlStream, documentFileName, _dataSourceName), CancellationToken.None);
+            }
+            catch (Exception ex)
             {
-                _logger.LogError(ex, $"Error occured while sending message to harvester queue");
+                _logger.LogError(ex, $"Error occured while harvesting source: {_dataSourceName}, file-id: {documentFileIdentifier}");
             }
         }
     }
@@ -66,9 +76,25 @@ public class JnccProcessor : IProcessor
             foreach (var anchorNode in anchorNodes)
             {
                 var hrefValue = anchorNode.GetAttributeValue("href", "");
-                documentLinks.Add("/" + hrefValue);
+                documentLinks.Add(hrefValue);
             }
         }
         return documentLinks;
+    }
+
+    private static string? GetFileIdentifier(string xmlString)
+    {
+        //Xml string to XElement Conversion
+        var xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(xmlString);
+        var xDoc = XDocument.Load(xmlDoc!.CreateNavigator()!.ReadSubtree());
+        var xmlElement = xDoc.Root;
+
+        string gmdNameSpaceString = "http://www.isotc211.org/2005/gmd";
+        var fileIdentifierXmlElement = xmlElement!.Descendants()
+                                                  .FirstOrDefault(n => n.Name.Namespace.NamespaceName == gmdNameSpaceString
+                                                                  && n.Name.LocalName == "fileIdentifier");
+        var fileIdentifier = fileIdentifierXmlElement?.Descendants()?.FirstOrDefault()?.Value;
+        return fileIdentifier;
     }
 }
